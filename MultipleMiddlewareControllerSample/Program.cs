7a631ea270deb468960f67d5b68d8e159cba7e51
Eser Şahin <eser.sahin@gmail.com>
2023-02-14 19:37:41 +0300

    1.Add Controller class and call from the middleware3 for In MultipleMiddlewareSample project, controller class and method to be called in Middleware3 have been added and called. 2.Added Multiple Middleware Controller Instance projects. In this project, the code is made to behave as if a real url call is being made.

---
+++ MultipleMiddlewareControllerSample/Program.cs
@@ -0,0 +1,98 @@
+﻿var pipeline = new List
+<
+    Func
+    <
+        Func<RequestContext, Task>,
+        Func<RequestContext, Task>
+    >
+>
+{
+    ExceptionMiddleware,
+    LogMiddleware
+};
+
+var routes = new Dictionary<string, Func<RequestContext, Task>?>
+{
+    { "/", IndexRoute },
+    { "/about", AboutRoute }
+};
+
+var requestContext = new RequestContext
+{
+    RequestUrl = "/"
+};
+
+var finalFunction = BuildPipeline(pipeline, routes);
+
+await finalFunction(requestContext);
+
+Console.WriteLine($"Response: {requestContext.Response}");
+
+static Func<RequestContext, Task> BuildPipeline
+(
+    List<Func<Func<RequestContext, Task>, Func<RequestContext, Task>>> middlewareList,
+    IReadOnlyDictionary<string, Func<RequestContext, Task>?> routeDict
+)
+{
+    Func<RequestContext, Task> finalFunction = async context =>
+    {
+        if (context.RequestUrl != null && routeDict.TryGetValue(context.RequestUrl, out var routeFunction))
+        {
+            await routeFunction?.Invoke(context)!;
+        }
+        else
+        {
+            context.Response = "404 Not Found";
+        }
+    };
+
+    foreach (var middleware in middlewareList)
+    {
+        finalFunction = middleware(finalFunction);
+    }
+
+    return finalFunction;
+}
+
+static Func<RequestContext, Task> LogMiddleware(Func<RequestContext, Task> next)
+{
+    return async context =>
+    {
+        await next(context);
+    };
+}
+
+static Func<RequestContext, Task> ExceptionMiddleware(Func<RequestContext, Task> next)
+{
+    return async context =>
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    };
+}
+
+static async Task IndexRoute(RequestContext context)
+{
+    Console.WriteLine($"IndexRoute: {context.RequestUrl}");
+    context.Response = "Index page";
+    await Task.CompletedTask;
+}
+
+static async Task AboutRoute(RequestContext context)
+{
+    Console.WriteLine($"AboutRoute: {context.RequestUrl}");
+    context.Response = "About page";
+    await Task.CompletedTask;
+}
+
+class RequestContext
+{
+    public string? RequestUrl { get; init; }
+    public string? Response { get; set; }
+}
--- MultipleMiddlewareSample/Program.cs
+++ MultipleMiddlewareSample/Program.cs
@@ -46,17 +46,19 @@ public class Middleware2
 public class Middleware3
 {
     private readonly Func<Task> _next;
+    private readonly Controller _controller;
 
-    public Middleware3(Func<Task> next)
+    public Middleware3(Func<Task> next, Controller controller)
     {
         _next = next;
+        _controller = controller;
     }
 
     public async Task InvokeAsync()
     {
         // Do something before calling the next middleware in the pipeline
         Console.WriteLine("Middleware 3: Before");
-
+        await _controller.MyMethod();
         await _next();
 
         // Do something after calling the next middleware in the pipeline
@@ -64,13 +66,23 @@ public class Middleware3
     }
 }
 
+public class Controller
+{
+    public async Task MyMethod()
+    {
+        Console.WriteLine("My method called.");
+        await Task.CompletedTask;
+    }
+}
+
 public class Program
 {
     public static async Task Main(string[] args)
     {
+        var controller = new Controller();
         var middleware1 = new Middleware1(() => Task.CompletedTask);
         var middleware2 = new Middleware2(() => Task.CompletedTask);
-        var middleware3 = new Middleware3(() => Task.CompletedTask);
+        var middleware3 = new Middleware3(() => Task.CompletedTask, controller);
 
         middleware2 = new Middleware2(middleware3.InvokeAsync);
         middleware1 = new Middleware1(middleware2.InvokeAsync);
